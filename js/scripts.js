2554543d90572fcfe497db6e34174c40f6caab7a
Calla Rudolph and Michelle Poterek <calla@>
2017-06-07 15:22:15 -0700

    change vowel variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //business logic
 
-var vowels = ["a", "e", "i", "o", "u", "y"];
+var vowels = ["a", "e", "i", "o", "u"];
 var manipulated = [];
 
 // if $(words).children("")
@@ -12,28 +12,19 @@ $(document).ready(function() {
   $("#pig-latin").submit(function(event) {
     event.preventDefault();
 
-    var input = $("#input").val();
-    var words = input.split(" ");
+var input = $("#input").val();
+var words = input.split(" ");
 
-      for (var i = 0; i < words.length; i += 1) {
-        for (var index = 0; index < vowels.length; index += 1) {
-          if (words[i].charAt(0) === vowels[index]) {
-          $("#result").append(words[i] + "ay")
-        }
-      }
+for (var i = 0; i < words.length; i += 1) {
+  for (var index = 0; index < vowels.length; index += 1) {
+    if (words[i].charAt(0) === vowels[index]) {
+      $("#result").append(words[i] + "ay")
     }
-
-
-
-        // if (words[i] === "a" ||words[i] === "e" || words[i] === "i" || words[i] === "o") {
-              // if (words[i] === vowels[i] ) {
-        console.log(words)
-      // }
-
-
-
-
-    // $("#result").text(words);
-
-  })
+    // } else {
+    //   var con = words.shift()
+    //   $("#result").append(words.concat(con));
+    // }
+  }
+}
+})
 });
