217bb0760c3bb23f0db13f649d902f01b6e6d175
Calla Rudolph and Michelle Poterek <calla@>
2017-06-07 14:19:37 -0700

    change vowel from i to index, add charAt, append ay

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,39 @@
+//business logic
+
+var vowels = ["a", "e", "i", "o", "u", "y"];
+var manipulated = [];
+
+// if $(words).children("")
+// firstChild(words)
+
+//user interface logic
+
 $(document).ready(function() {
   $("#pig-latin").submit(function(event) {
     event.preventDefault();
 
-    var words = $("#input").val();
-    console.log(words)
+    var input = $("#input").val();
+    var words = input.split(" ");
 
+      for (var i = 0; i < words.length; i += 1) {
+        for (var index = 0; index < vowels.length; index += 1) {
+          if (words[i].charAt(0) === vowels[index]) {
+          $("#result").append(words[i] + "ay")
+        }
+      }
+    }
+
+
+
+        // if (words[i] === "a" ||words[i] === "e" || words[i] === "i" || words[i] === "o") {
+              // if (words[i] === vowels[i] ) {
+        console.log(words)
+      // }
+
+
+
+
+    // $("#result").text(words);
 
   })
 });
